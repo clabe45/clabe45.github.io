58016bd77d2a21d91f09cf2e0b1af61ed804a89e
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-09-24 15:01:55 -0400

    Updates

---
+++ precache-manifest.edc47ed4abe6201fb115e7ec532e0e46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e3e38a81764222902be2f502ea543d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d12432095475582b270",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "22febd48ab27d1fe0c4d",
+    "url": "/static/css/main.f35b4ccd.chunk.css"
+  },
+  {
+    "revision": "9d12432095475582b270",
+    "url": "/static/js/2.8c6d476e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8c6d476e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22febd48ab27d1fe0c4d",
+    "url": "/static/js/main.9c0b492c.chunk.js"
+  },
+  {
+    "revision": "fc1e0d2b13c962153933",
+    "url": "/static/js/runtime-main.e466701c.js"
+  }
+]);
