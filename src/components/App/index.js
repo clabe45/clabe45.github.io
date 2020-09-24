6ff58ef7140497bc2777ac89b6e5d8d03c690799
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-09-24 09:41:29 -0400

    Remove Navbar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Navbar from '../Navbar/index.js'
 import Home from '../Home/index.js'
 import About from '../About/index.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,24 +9,16 @@ function App() {
   return (
     <div className="app-wrapper">
       <Router>
-        <Row>
-          <Col className="navbar-column">
-            <Navbar/>
-          </Col>
-
-          <Col>
-            <Switch>
-              <div className="text-center">
-                <Route path="/about">
-                  <About />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </div>
-            </Switch>
-          </Col>
-        </Row>
+        <Switch>
+          <div className="text-center">
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </div>
+        </Switch>
       </Router>
     </div>
   )
