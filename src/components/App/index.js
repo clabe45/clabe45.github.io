9f72d273dffaf3b51e6d700c2182294baa6c6fbb
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-09-23 23:38:34 -0400

    Move App files into a new component directory

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Navbar from './components/Navbar/index.js'
-import Home from './components/Home/index.js'
+import Navbar from '../Navbar/index.js'
+import Home from '../Home/index.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
