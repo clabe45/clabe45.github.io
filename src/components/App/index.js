de10c4b66011d8319e81c7a5788367ecc3653bd4
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-09-24 09:47:17 -0400

    No longer use router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from '../Home/index.js'
-import About from '../About/index.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
 function App() {
   return (
     <div className="app-wrapper">
-      <Router>
-        <Switch>
-          <div className="text-center">
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </div>
-        </Switch>
-      </Router>
+      <div className="text-center">
+        <Home />
+      </div>
     </div>
   )
 }
