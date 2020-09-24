2303f2f778ffc62c15cc89730c6444408ad6c278
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2020-09-23 21:05:31 -0400

    Set up router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Navbar from './components/Navbar/index.js'
+import Home from './components/Home/index.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-  return <Navbar/>
+  return (
+    <div className="app-wrapper">
+      <Router>
+        <Row>
+          <Col className="navbar-column">
+            <Navbar/>
+          </Col>
+
+          <Col>
+            <Switch>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
