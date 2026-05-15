12668d6ba648f2653d40992f17c9a9dade98f408
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2026-05-14 20:59:36 -0700

    feat!: remove obsolete redirects to etro docs
    The docs were previously moved to etrojs.dev.

--- vidar-redirect/redirect.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function () {
-  const page = window.location.toString().substring(
-    'https://calebsacks.me/vidar/'.length
-  );
-
-  window.location.replace(`https://etrojs.dev/${page}`);
-});
